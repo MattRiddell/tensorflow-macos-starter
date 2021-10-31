edad66b799fed06cb8375f3ae90daa51354c6fe7
Matthew Riddell <matt@neogen.ai>
2021-10-31 15:02:14 -0400

    More tests

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 import tensorflow as tf 
+# To log device placement
+# tf.debugging.set_log_device_placement(True)
+
+print("TensorFlow version:", tf.__version__)
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-print(tf.config.experimental.list_physical_devices());
+print("Device List: ",tf.config.experimental.list_physical_devices());
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+predictions = model(x_train[:1]).numpy()
+print(predictions)
